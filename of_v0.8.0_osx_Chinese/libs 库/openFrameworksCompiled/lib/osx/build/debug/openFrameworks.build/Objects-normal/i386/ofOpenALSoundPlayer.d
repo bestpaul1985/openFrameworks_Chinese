38f35c6cef3c781f2ce15b9eab6daa2dff5ed245
Peng Cheng <bestpaul1985@gmail.com>
2014-03-17 18:48:29 -0400

    update

--- of_v0.8.0_osx_Chinese/libs 库/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofOpenALSoundPlayer.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/pengcheng/Dropbox/of_v0.8.0_osx_Chinese/libs/openFrameworks/sound/ofOpenALSoundPlayer.cpp \
-  ../../../../libs/openFrameworks/sound/ofOpenALSoundPlayer.h \
-  /Users/pengcheng/Dropbox/of_v0.8.0_osx_Chinese/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
-  ../../../../libs/glew/include/GL/glew.h \
-  ../../../../libs/tess2/include/tesselator.h
--- of_v0.8.0_osx_Chinese/libs 库/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofQTKitMovieRenderer.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pengcheng/Dropbox/of_v0.8.0_osx_Chinese/libs/openFrameworks/video/ofQTKitMovieRenderer.m \
-  ../../../../libs/openFrameworks/video/ofQTKitMovieRenderer.h
