7ff0e7387e33d810fa6e437db0b39b87e70fe74c
Paul Cheng <bestpaul1985@gmail.com>
2014-06-08 10:08:16 -0400

    update

--- of_v0.8.1_osx_Chinese/examoles/math 数学/trigonometryExample/src/ofApp.cpp
+++ of_v0.8.1_osx_Chinese/examoles/math 数学/trigonometryExample/src/ofApp.cpp
@@ -1,3 +1,4 @@
+//04-25-2014
 /**
  *
  * OFDevCon Example Code Sprint
@@ -6,15 +7,18 @@
  * Created by Roy Macdonald on 2/24/2012
  */
 
+//三角函数例子
 #include "ofApp.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 	
+    //设置证绿和背景颜色
 	ofSetFrameRate(60);
 	ofBackground(50);
-		
-	//initial values
+    
+    
+    //一些有关的初始数字（包括角度，半径，旋转速度，中心)，都在 ofApp.h 里宣布的
 	angle=0;
 	cosine=0;
 	sine=0;
@@ -23,9 +27,12 @@ void ofApp::setup(){
 	radius = 180;
 	center.set(ofGetWidth()*0.3f, ofGetHeight()*0.6f, 0);
 	
-	ofSetCircleResolution(40);
+	
+    //设置圆圈分辨率
+    ofSetCircleResolution(40);
 	
 	
+    //弧的一些性质, setFill() 控制是否填充颜色
 	angleArc.setArcResolution(360);
 	angleArc.setFilled(true);
 	angleArc.setColor(ofColor(240, 130, 10));
@@ -35,24 +42,26 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	if (!ofGetMousePressed()) {//press the mouse to stop the angle from incrementing.
-		angle+=rotationSpeed;//at each update the angle get's incremented 
+    //用鼠标控来制运动， 都挺明显的
+	if (!ofGetMousePressed()) {
+		angle+=rotationSpeed;
 	}
-	if (angle>=TWO_PI) { //if the angle is more than or equal to two PI (a full rotation measured in Radians) then make it zero.
+	if (angle>=TWO_PI) {
 		angle=0;
 	}
-
-	//here we get the sine and cosine values for the angle
+    
+    
+    //计算余弦, 正弦, 切线
 	cosine=cos(angle);
 	sine=sin(angle);
 	tangent=tan(angle);
 	
 	
-	point.set(cosine * radius, sine * radius, 0);//here we set the cyan circle position
-	
-	//this is just to draw the arc that represents the angle 
+	point.set(cosine * radius, sine * radius, 0);
+    
+    //一个代表角度的弧
 	angleArc.clear();
-	angleArc.arc( 0,  0, radius * 0.5f, radius * 0.5f, 0, ofRadToDeg(angle)); 
+	angleArc.arc( 0,  0, radius * 0.5f, radius * 0.5f, 0, ofRadToDeg(angle));
 	angleArc.close();
 	
 	
@@ -60,22 +69,22 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	//we translate everything to the center of the circle so it's easier to draw and understand.
+    
+    //draw()永远是从左上角开始的，我们先移动到中间，这样作为启发点会方便一些
 	ofTranslate(center);
 	
-	//draw the yellow circle
+    //画一个黄色的圆圈。 位置 -（0，0）半径 - 180 像素
 	ofSetColor(240, 230, 10);
 	ofCircle(0,0, radius);
 	
-	//draw the cyan circle that's moving over the yellow circle's perimeter 
+    //再画一个青色的圆圈， 差不多的概念
 	ofSetColor(0, 140, 255);
 	ofCircle(point, 10);
 	
-	//draw the orange arc that represents the angle
+    //画桔黄色的弧
 	angleArc.draw();
-	
-	
-	//draw the axis lines
+    
+    //画x轴和y轴
 	ofSetColor(20);
 	ofSetLineWidth(1);
 	ofLine(-radius * 1.3f, 0, radius * 2, 0);
@@ -87,7 +96,7 @@ void ofApp::draw(){
 	ofLine(-radius, 0 ,-radius,  -radius * 2);
 	ofLine( radius, 0 , radius,  -radius * 2);
 	
-	//draw the pink bars
+    //画粉红色的杆
 	ofSetColor(255, 0, 127);
 	ofRect(0, -radius * 2, cosine * radius, 20);
 	ofRect(radius * 2, 0, -20, sine * radius);
@@ -96,29 +105,30 @@ void ofApp::draw(){
 	ofSetLineWidth(3);
 	
 	
-	//draw lines from the cyan ball to the pink bars
+    //从青色到粉红色杆的直线
 	ofSetColor(255, 0, 50);
 	ofLine(point.x, point.y, cosine * radius, -radius * 2);
 	ofLine(point.x, point.y, radius * 2, sine * radius);
 	
-	//draw the line from the yellow circle's center to the cyan circle 
+    
+    //画旋转的半径
 	ofSetColor(0, 127, 255);
 	ofLine(0, 0, point.x, point.y);
 	
-
-	//draw the dark lines inside the yellow circle that represent the distance from the axis to the cyan circle
+    
+    //画移动的黑线
 	ofSetColor(40);
 	ofLine(0, sine * radius, cosine * radius, sine * radius);
 	ofLine(cosine * radius, 0, cosine * radius, sine * radius);
 	
-
-	// draw the info text
+    
+    //画不断更新的文字包括角度，半径，余弦, 正弦, 切线
 	ofSetColor(10);
 	ofDrawBitmapString("Angle (RAD): " + ofToString(angle), 3,-3);
 	ofSetColor(30);
 	ofDrawBitmapString("Angle (DEG): " + ofToString(ofRadToDeg(angle)), 3,20);
 	
-	ofDrawBitmapString("Radius: " + ofToString(radius), -radius +20, -3);	
+	ofDrawBitmapString("Radius: " + ofToString(radius), -radius +20, -3);
 	
 	ofSetColor(220);
 	ofDrawBitmapString("Angle Sine: " + ofToString(sine), radius *2 +3, 0);
@@ -131,32 +141,34 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
+    //点鼠标将导致重新设置动画
 	ofVec2f mousePos(x-center.x, y-center.y);
 	if (mousePos.length()<radius ) {
 		angle = -mousePos.angleRad(ofVec2f(radius,0));
 		if (angle<0) {
 			angle+=TWO_PI;
 		}
-	}	
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+    //点鼠标将导致重新设置动画
 	ofVec2f mousePos(x-center.x, y-center.y);
 	if (mousePos.length()<radius ) {
 		angle = -mousePos.angleRad(ofVec2f(radius,0));
@@ -168,20 +180,20 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
--- of_v0.8.1_osx_Chinese/examoles/math 数学/vectorMathExample/src/ofApp.cpp
+++ of_v0.8.1_osx_Chinese/examoles/math 数学/vectorMathExample/src/ofApp.cpp
@@ -1,139 +1,153 @@
+//04-25-2014
 #include "ofApp.h"
 
+//如何通过操纵一个不断旋转的坐标轴来绘制一个三维图形
 //--------------------------------------------------------------
 void ofApp::setup(){
+	//ofSetVerticalSync() 好像同步更新屏幕重绘和屏幕的垂直刷新
 	ofSetVerticalSync(true);
-
+    
+	//设置旋转量和速度
 	nPts = 0;
 	rotateAmount.set(0,0,1);
 	speedOfRotation = 0.5f;
-
+    
 	ofBackground(255,255,255);
-
+	//定义中心
 	ofVec3f center(ofGetWidth()/2, ofGetHeight()/2, 0);
-
+    
+	//设置一些坐标，xAxisMin, xAxisMax, yAxisMin, yAxisMax, zAxisMin, zAxisMax 都是ofVec3f
+	//ofVec3f有三个变量， 很善于存储三维属性，比如说位置，速度，和加速度，
 	xAxisMin.set(-100,0,0);
 	xAxisMax.set(100,0,0);
 	yAxisMin.set(0,-100,0);
 	yAxisMax.set(0,100,0);
 	zAxisMin.set(0,0,-100);
 	zAxisMax.set(0,0,100);
-
+    
 	xAxisMin += center;
 	xAxisMax += center;
 	yAxisMin += center;
 	yAxisMax += center;
 	zAxisMin += center;
 	zAxisMax += center;
-
-	bDrawnAnything = false;        // added this cause theo saw this example
-								// and was like "how do I tell zach that a rotating
-								// cross is not that interesting ??"  before realizing
-								// that one should draw :)
-
+    
+	bDrawnAnything = false;     // added this cause theo saw this example
+    // and was like "how do I tell zach that a rotating
+    // cross is not that interesting ??"  before realizing
+    // that one should draw :)
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
-     // rotation occurs around 0,0,0 position.
-     // since 0,0,0 is the top left corner, we need to:
-     // (A) move over,
-     // (B) rotate
-     // (C) move back
-     // this could all be one for loop, but broken out here for
-     // clarity on the steps:
-
-     ofVec3f center(ofGetWidth()/2, ofGetHeight()/2,0);
-
-     // move the points so that their center (ofGetW/2, ofGetH/2) is at 0,0,0
-     for (int i = 0; i < nPts; i++){
+    
+    // rotation occurs around 0,0,0 position.
+    // since 0,0,0 is the top left corner, we need to:
+    // (A) move over,
+    // (B) rotate
+    // (C) move back
+    // this could all be one for loop, but broken out here for
+    // clarity on the steps:
+    
+    //别忘了启发点永远在OF屏幕的左上角（0，0）而我们旋转的中心在屏幕的中心，所以需要移动到屏幕中心，旋转，然后回去。
+    //其实可以用ofPushMatrix()/ofPushMatrix()得到一样的效果，但这个一步一步比较明确
+    ofVec3f center(ofGetWidth()/2, ofGetHeight()/2,0);
+    
+    //move the points so that their center (ofGetW/2, ofGetH/2) is at 0,0,0
+    for (int i = 0; i < nPts; i++){
         pts[i]-= center;
-     }
-
-     // rotate the points
-     for (int i = 0; i < nPts; i++){
+    }
+    
+    //rotate the points
+    //旋转所有的顶点
+    for (int i = 0; i < nPts; i++){
         pts[i] = pts[i].rotated(speedOfRotation, rotateAmount);
-     }
-
-     // move them back
-     for (int i = 0; i < nPts; i++){
+    }
+    
+    //move them back
+    for (int i = 0; i < nPts; i++){
         pts[i]+= center;
-     }
-
-
-     xAxisMin -= center;
-     xAxisMax -= center;
-     yAxisMin -= center;
-     yAxisMax -= center;
-     zAxisMin -= center;
-     zAxisMax -= center;
-
-
-     xAxisMin.rotate(speedOfRotation, rotateAmount);
-     xAxisMax.rotate(speedOfRotation, rotateAmount);
-     yAxisMin.rotate(speedOfRotation, rotateAmount);
-     yAxisMax.rotate(speedOfRotation, rotateAmount);
-     zAxisMin.rotate(speedOfRotation, rotateAmount);
-     zAxisMax.rotate(speedOfRotation, rotateAmount);
-
-     xAxisMin += center;
-     xAxisMax += center;
-     yAxisMin += center;
-     yAxisMax += center;
-     zAxisMin += center;
-     zAxisMax += center;
+    }
+    
+    //移动的方式都挺明显的
+    xAxisMin -= center;
+    xAxisMax -= center;
+    yAxisMin -= center;
+    yAxisMax -= center;
+    zAxisMin -= center;
+    zAxisMax -= center;
+    
+    xAxisMin.rotate(speedOfRotation, rotateAmount);
+    xAxisMax.rotate(speedOfRotation, rotateAmount);
+    yAxisMin.rotate(speedOfRotation, rotateAmount);
+    yAxisMax.rotate(speedOfRotation, rotateAmount);
+    zAxisMin.rotate(speedOfRotation, rotateAmount);
+    zAxisMax.rotate(speedOfRotation, rotateAmount);
+    
+    xAxisMin += center;
+    xAxisMax += center;
+    yAxisMin += center;
+    yAxisMax += center;
+    zAxisMin += center;
+    zAxisMax += center;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
-     ofSetHexColor(0x000000);
-     ofNoFill();
-     ofBeginShape();
-     for (int i = 0; i < nPts; i++){
-         ofVertex(pts[i].x, pts[i].y);
-     }
-     ofEndShape();
-
-     string info = "speed of rotation (a/z): " + ofToString(speedOfRotation,3) + "\n" +
-                  "rotateAmount in x (s/x): " + ofToString(rotateAmount.x,3) + "\n" +
-                  "rotateAmount in y (d/c): " + ofToString(rotateAmount.y,3) + "\n" +
-                  "rotateAmount in z (f/v): " + ofToString(rotateAmount.z,3);
-
-     ofFill();
-     ofSetHexColor(0xE5A93F);
-     ofRect(10,10,300,70);
-     ofSetHexColor(0x000000);
-     ofDrawBitmapString(info,30,30);
-
-
-     ofSetColor(50,50,255);
-     ofNoFill();
-     ofBeginShape();
-        ofVertex(xAxisMin.x, xAxisMin.y, xAxisMin.z);
-        ofVertex(xAxisMax.x, xAxisMax.y, xAxisMax.z);
-     ofEndShape();
-     ofBeginShape();
-        ofVertex(yAxisMin.x, yAxisMin.y, yAxisMin.z);
-        ofVertex(yAxisMax.x, yAxisMax.y, yAxisMax.z);
-     ofEndShape();
-     ofBeginShape();
-        ofVertex(zAxisMin.x, zAxisMin.y, zAxisMin.z);
-        ofVertex(zAxisMax.x, zAxisMax.y, zAxisMax.z);
-     ofEndShape();
-
-
+    
+    //设置背景颜色，但表填充，制造顶点
+    ofSetHexColor(0x000000);
+    ofNoFill();
+    ofBeginShape();
+    for (int i = 0; i < nPts; i++){
+        ofVertex(pts[i].x, pts[i].y);
+    }
+    ofEndShape();
+    
+    //显示三个方向的旋转度
+    string info = "speed of rotation (a/z): " + ofToString(speedOfRotation,3) + "\n" +
+    "rotateAmount in x (s/x): " + ofToString(rotateAmount.x,3) + "\n" +
+    "rotateAmount in y (d/c): " + ofToString(rotateAmount.y,3) + "\n" +
+    "rotateAmount in z (f/v): " + ofToString(rotateAmount.z,3);
+    
+    
+    //左上角的桔黄色方块
+    ofFill();
+    ofSetHexColor(0xE5A93F);
+    ofRect(10,10,300,70);
+    ofSetHexColor(0x000000);
+    ofDrawBitmapString(info,30,30);
+    
+    
+    //画线，这是openGL的构造方式
+    ofSetColor(50,50,255);
+    ofNoFill();
+    ofBeginShape();
+    ofVertex(xAxisMin.x, xAxisMin.y, xAxisMin.z);
+    ofVertex(xAxisMax.x, xAxisMax.y, xAxisMax.z);
+    ofEndShape();
+    ofBeginShape();
+    ofVertex(yAxisMin.x, yAxisMin.y, yAxisMin.z);
+    ofVertex(yAxisMax.x, yAxisMax.y, yAxisMax.z);
+    ofEndShape();
+    ofBeginShape();
+    ofVertex(zAxisMin.x, zAxisMin.y, zAxisMin.z);
+    ofVertex(zAxisMax.x, zAxisMax.y, zAxisMax.z);
+    ofEndShape();
+    
+	//刚开始的提示
     if (bDrawnAnything == false){
         ofSetColor(0,0,0);
         ofDrawBitmapString("draw something!", ofGetWidth()/2 + 100,ofGetHeight()/2 + 20);
     }
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed  (int key){
-
+    
+	//慢慢移动三个方向的坐标轴,也可以加速
     switch (key){
         case 'a':
             speedOfRotation += 0.05f;
@@ -145,19 +159,19 @@ void ofApp::keyPressed  (int key){
             rotateAmount.x += 0.05f;
             break;
         case 'x':
-             rotateAmount.x -= 0.05f;
+            rotateAmount.x -= 0.05f;
             break;
         case 'd':
             rotateAmount.y += 0.05f;
             break;
         case 'c':
-             rotateAmount.y -= 0.05f;
+            rotateAmount.y -= 0.05f;
             break;
         case 'f':
             rotateAmount.z += 0.05f;
             break;
         case 'v':
-             rotateAmount.z -= 0.05f;
+            rotateAmount.z -= 0.05f;
             break;
     }
 }
@@ -172,37 +186,38 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
-     bDrawnAnything = true;
-
-     if (nPts < MAX_N_PTS){
+    
+    bDrawnAnything = true;
+    //托运鼠标到达的每一点都被添加为新的顶点，
+    if (nPts < MAX_N_PTS){       //MAX_N_PTS 被定义为1500在ofApp.h里
         pts[nPts].x = x;
         pts[nPts].y = y;
         nPts++;
-     }
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-     nPts = 0;
+    //删除然后重新开始
+    nPts = 0;
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
